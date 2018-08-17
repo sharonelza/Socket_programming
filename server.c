1e0e3cfa3dec6018416a076e2e28fbcc1af8b99f
shar <sharonelza96@gmail.com>
2018-08-17 13:34:02 +0530

    Initial commit

---
+++ server.c
@@ -0,0 +1,84 @@
+/*Name : Jay Jagtap
+ 3154037
+ Problem Statement: To achieve File transfer using TCP/IP Protocol
+ */
+
+/*
+ Server Side
+ Please pass port no as command line argument
+ */
+#include <stdio.h>
+#include <stdlib.h>    // Used for exit function
+#include <string.h>    //Used for bzero function
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <ctype.h>
+
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char buffer[512];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);          /*Socket function is used to create a socket
+                                                        The above code will create a socket of
+                                                        Address Family : AF_INET (this is IP version 4)
+                                                        Type : SOCK_STREAM (this means connection oriented TCP protocol)
+                                                        Protocol : 0 [ or IPPROTO_TCP , IPPROTO_UDP ] */
+    
+    
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;                //The sockaddr_in has a member called sin_addr
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+             sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd,
+                       (struct sockaddr *) &cli_addr,
+                       &clilen);
+    if (newsockfd < 0)
+        error("ERROR on accept");
+    
+    
+    
+    FILE *fp;
+    int ch = 0;
+    fp = fopen("glad_receive.txt","a");
+    int words;
+    read(newsockfd, &words, sizeof(int));
+    
+    while(ch != words)
+    {
+        read(newsockfd , buffer , 512);
+        fprintf(fp , " %s" , buffer);
+        
+        ch++;
+    }
+    printf("The file was received successfully\n");
+    printf("The new file created is glad5.txt");
+    close(newsockfd);
+    close(sockfd);             //It is used to close the socket
+    return 0;
+}
+
+
